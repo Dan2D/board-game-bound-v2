89341ded400042e8310b2e998909272556324387
Dan Schultz <travelguy90@gmail.com>
2019-09-04 14:39:32 -0700

    Updates

---
+++ precache-manifest.5df0b88943464c4c433884ada1f328cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef33bc287af9ab1da41a77c9c878ab83",
+    "url": "/board-game-bound-v2/index.html"
+  },
+  {
+    "revision": "ae06c884ac17650d3b4c",
+    "url": "/board-game-bound-v2/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "744f706497b2fec87375",
+    "url": "/board-game-bound-v2/static/css/main.b1c576c0.chunk.css"
+  },
+  {
+    "revision": "ae06c884ac17650d3b4c",
+    "url": "/board-game-bound-v2/static/js/2.f181bc99.chunk.js"
+  },
+  {
+    "revision": "744f706497b2fec87375",
+    "url": "/board-game-bound-v2/static/js/main.77baa7da.chunk.js"
+  },
+  {
+    "revision": "568f22e4a084982dc650",
+    "url": "/board-game-bound-v2/static/js/runtime~main.5176f877.js"
+  },
+  {
+    "revision": "1e49d7a15f1f18719748d6fa2778364a",
+    "url": "/board-game-bound-v2/static/media/bg-overlay.1e49d7a1.png"
+  },
+  {
+    "revision": "340147ed1a09457079fb8a5591149f40",
+    "url": "/board-game-bound-v2/static/media/loading-icon.340147ed.png"
+  },
+  {
+    "revision": "3fa4654e364e7afbebde67c7fa346343",
+    "url": "/board-game-bound-v2/static/media/pattern-bg.3fa4654e.gif"
+  }
+]);
